89d4e1f29fafaf2e0abe9611c021f92cc20c1ffe
Kelvin <haminhhoang19@gmail.com>
2025-04-19 22:38:58 +1000

    linked_list

--- s.py
+++ s.py
@@ -1,3 +1,16 @@
-print("hello world")
-print("testingsss")
+#COMP10001 TESTING & STUDY
+class Node:
+    def __init__(self, data=None) -> None:
+        self.data = data
+        self.next = None
+class Linkedlist:
+    def __init__(self) -> None:
+        self.head = Node()
+    def append(self, data):
+        new_node = Node(data)
+        cur = self.head
+        while cur.next != None:
+            cur = cur.next
+        cur.next = new_node
 
+  
