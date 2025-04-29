3b15580fd511d7ca5f7d717c0f01425cf90fdafa
Kelvin <haminhhoang19@gmail.com>
2025-04-29 10:40:54 +1000

    updated stack&queue func

--- stack_queue.py
+++ stack_queue.py
@@ -9,6 +9,44 @@ class Stack:
         self.top = None
         self.size = 0
     
+    def repr(self):
+        items = []
+        cur = self.top
+        while cur != None:
+            items.append(cur.value)
+            cur = cur.next
+        return items
+
     def len(self):
-        pass
+        return self.size
+        # O(1) - constant time
+    def push(self, value):
+        new_node = Node(value)
+        new_node.next = self.top
+        self.top = new_node
+
+        self.size += 1
     
+    def pop(self):
+        if self.top is None:
+            print("stack is empty")
+        pop_value = self.top.value
+        self.top = self.top.next
+
+        self.size -= 1
+        return pop_value
+    def peek(self):
+        return self.top.value
+        
+if __name__ == '__main__':
+    stack = Stack()
+    stack.push(10)
+    stack.push(14)
+    stack.push(20)
+    print(stack.repr())
+    print(stack.peek())
+    print(stack.pop())
+    print(stack.repr())
+    print(stack.len())
+
+
