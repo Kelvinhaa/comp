02778afdadb71e57b0bc4a20a891b48613e104f5
Kelvin <haminhhoang19@gmail.com>
2025-04-29 10:05:22 +1000

    adding stack_queue file

---
+++ stack_queue.py
@@ -0,0 +1,14 @@
+#Learning stack and queue here
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+
+class Stack:
+    def __init__(self) -> None:
+        self.top = None
+        self.size = 0
+    
+    def len(self):
+        pass
+    
