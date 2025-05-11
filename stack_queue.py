90d3b0ff854e54ce9cc3f9dd1842dca39abec19c
Kelvin <haminhhoang19@gmail.com>
2025-05-11 17:07:31 +1000

    added Queue

--- stack_queue.py
+++ stack_queue.py
@@ -50,3 +50,58 @@ if __name__ == '__main__':
     print(stack.len())
 
 
+
+class Queue:
+    def __init__(self):
+        self.front = None  # First to come out
+        self.rear = None   # Last to go in
+        self.size = 0
+
+    def is_empty(self):
+        return self.size == 0
+
+    def enqueue(self, value):
+        new_node = Node(value)
+        if self.rear is None:
+            self.front = self.rear = new_node
+        else:
+            self.rear.next = new_node
+            self.rear = new_node
+        self.size += 1
+
+    def dequeue(self):
+        if self.is_empty():
+            return None  # or raise an error
+        removed = self.front
+        self.front = self.front.next
+        if self.front is None:  # If the queue is now empty
+            self.rear = None
+        self.size -= 1
+        return removed.value
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        return self.front.value
+
+    def __len__(self):
+        return self.size
+
+    def __repr__(self):
+        values = []
+        current = self.front
+        while current:
+            values.append(str(current.value))
+            current = current.next
+        return " -> ".join(values)
+
+# Example usage
+if __name__ == '__main__':
+    q = Queue()
+    q.enqueue(10)
+    q.enqueue(20)
+    q.enqueue(30)
+    print(q)             # 10 -> 20 -> 30
+    print(q.dequeue())   # 10
+    print(q.peek())      # 20
+    print(len(q))        # 2
