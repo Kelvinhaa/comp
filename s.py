13e34bb7a99f27ec79f294aac6d655e7142bfdbc
Kelvin <haminhhoang19@gmail.com>
2025-05-12 11:49:11 +1000

    Added binary search tree

--- s.py
+++ s.py
@@ -6,6 +6,16 @@ class Node:
 class Linkedlist:
     def __init__(self) -> None:
         self.head = Node()
+    def __contains__(self, data):
+
+        cur = self.head
+        while cur.next != None:
+            if cur.data == data:
+                return True
+            cur = cur.next
+        return False
+    def length(self):
+        pass
     def append(self, data):
         new_node = Node(data)
         cur = self.head
@@ -19,4 +29,10 @@ class Linkedlist:
         new_node = Node(data)
         new_node.next = self.head
         self.head = new_node
-    
+
+ll = Linkedlist()
+ll.append(10)
+ll.append(5)
+ll.append(18)
+ll.prepend(100)
+print(ll)
