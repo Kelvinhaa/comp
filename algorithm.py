ff83698c8ef4f3d863e954626e7408f4eccad3d8
Kelvin <haminhhoang19@gmail.com>
2025-06-22 15:06:23 +1000

    added quicksort

--- algorithm.py
+++ algorithm.py
@@ -13,5 +13,34 @@ def selection_sort(arr):
 
 # print(selection_sort([1, 2, 9, 3, 4, 10]))
 
+
+def quick_sort(arr, low, high):
+    if low < high:
+        partition_pos = partition(arr, low, high)
+        quick_sort(arr, low, partition_pos - 1)
+        quick_sort(arr, partition_pos + 1, high)
+    return arr
+def partition(arr, low, high):
+    i = low
+    j = high - 1
+    pivot = arr[high]
+    while i < j:
+        while i < high and arr[i] < pivot:
+            i += 1
+        while j > low and arr[j] > pivot:
+            j -= 1
+        if i < j:
+            arr[i], arr[j] = arr[j], arr[i]
+    if arr[i] > pivot:
+        arr[i], arr[high] = arr[high], arr[i]
+    return i
+
+# arr = [2, 1, 8, 6, 5, 7, 3, 4]
+# print(quick_sort(arr, 0, len(arr) - 1))
+
+        
+
+
 if __name__ == '__main__':
-    assert selection_sort([1, 2, 9, 3, 4, 5, 11, 10]) == [1, 2, 3, 4, 5, 9, 10, 11]  
+assert selection_sort([1, 2, 9, 3, 4, 5, 11, 10]) == [1, 2, 3, 4, 5, 9, 10, 11]  
+
